6ab9b32b971ae9b84a5d8c58242bbed80c90befc
ganpol <ganpol@gmail.com>
2022-01-21 20:59:32 +0800

    123

--- NewsPortal/News/models.py
+++ NewsPortal/News/models.py
@@ -1,41 +1,71 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Author(models.Model):
-    full_name = models.CharField(max_length=255)
-    position = models.CharField(max_length=255)
-    labor_contract = models.IntegerField()
-    user = models.OneToOneField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rate = models.IntegerField(default=0)
 
-class Users(models.Model)
-    pass
 
 class Category(models.Model):
     category_name = models.CharField(max_length=255, unique=True)
-    # price = models.FloatField(default=0.0)
-    # composition = models.TextField(default="Состав не указан")
 
 
 class Post(models.Model):
+
+
+
+
+
     time = models.DateTimeField(auto_now_add=True)
     news_text = models.CharField()
     news_rate = models.IntegerField(default=1)
     news_title = models.CharField()
-    p_chose = models.
+    p_chose = models.CharField(max_length = 1, choices = POSTS, default = article)
+    rate = models.IntegerField(default=0)
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, through='PostCategory')
+
+    def like(self):
+        self.rate += 1
+        self.save()
+
+    def dislike(self):
+        self.rate -= 1
+        self.save()
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model)
+    comments = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    rate = models.IntegerField(default=0)
+
+    posts = models.ForeignKey(Post, through='PostCategory')
+    users = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def like(self):
+        self.rate += 1
+        self.save()
+
+    def dislike(self):
+        self.rate -= 1
+        self.save()
+
+    # position = models.CharField(max_length=255)
+    # labor_contract = models.IntegerField()
 
     # time_out = models.DateTimeField(null=True)
     # cost = models.FloatField(default=0.0)
     # take_away = models.BooleanField(default=False)
     # complete = models.BooleanField(default=False)
 
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    posts = models.ForeignKey(Category, through='PostCategory')
+    # price = models.FloatField(default=0.0)
+    # composition = models.TextField(default="Состав не указан")
 
-
-class PostCategory(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    amount = models.IntegerField(default=1)
-
-class Comment(models.Model)
-    pass
+    # amount = models.IntegerField(default=1)
